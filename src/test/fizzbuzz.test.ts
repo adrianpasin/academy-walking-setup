ffc7fc63d4185d481e6c25e5f2d850d0776e7059
Adrian Pasin <adrian.pasin@cazoo.co.uk>
2021-12-16 10:31:44 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -1,8 +1,16 @@
 import {Fizzbuzz} from "../main/fizzbuzz";
 
 describe('when fizzbuzzing', () => {
-    it("should return a string representation of the number", () => {
+    it("should return '1' as a representation of the number 1", () => {
         let fizzbuzz: Fizzbuzz = new Fizzbuzz();
         expect(fizzbuzz.convert(1)).toBe('1');
+    });
+    it("should return '2' as a representation of the number 2", () => {
+        let fizzbuzz: Fizzbuzz = new Fizzbuzz();
+        expect(fizzbuzz.convert(2)).toBe('2');
+    });
+    it("should return 'Fizz' as a representation of the number 3", () => {
+        let fizzbuzz: Fizzbuzz = new Fizzbuzz();
+        expect(fizzbuzz.convert(3)).toBe('Fizz');
     })
 })
