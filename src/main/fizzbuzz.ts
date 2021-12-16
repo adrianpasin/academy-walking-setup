07c3cb9e00257cfbba9a0bd633987a17f13edef1
Mahijani <mahima.jani@cazoo.com>
2021-12-16 16:26:38 +0530

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,5 +1,8 @@
 export class Fizzbuzz {
     convert(input: number): string {
+        if(input % 3 === 0){
+            return 'Fizz';
+        }
         return `${input}`;
     }
     
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -1,16 +1,15 @@
 import {Fizzbuzz} from "../main/fizzbuzz";
 
 describe('when fizzbuzzing', () => {
-    it("should return '1' as a representation of the number 1", () => {
-        let fizzbuzz: Fizzbuzz = new Fizzbuzz();
-        expect(fizzbuzz.convert(1)).toBe('1');
-    });
-    it("should return '2' as a representation of the number 2", () => {
-        let fizzbuzz: Fizzbuzz = new Fizzbuzz();
-        expect(fizzbuzz.convert(2)).toBe('2');
-    });
-    it("should return 'Fizz' as a representation of the number 3", () => {
-        let fizzbuzz: Fizzbuzz = new Fizzbuzz();
-        expect(fizzbuzz.convert(3)).toBe('Fizz');
-    })
+    it.each(
+        [
+            ['1', 1],
+            ['2', 2],
+            ['Fizz', 3],
+            ['4', 4], 
+            ['Buzz', 5]
+        ])('should return %s as a representation of the number %i', (expectedOutput, input)=> {
+            let fizzbuzz: Fizzbuzz = new Fizzbuzz();
+            expect(fizzbuzz.convert(input)).toBe(expectedOutput);
+        });
 })
